d4d747463049dbf5652291d4ffab834c7e556592
Tanvanat <tanvanat112@gmail.com>
2025-08-27 17:49:23 +0700

    openstack: switch to IDs (image/flavor/network), use keypair Fronttest

--- kubernetes/fluxcd/repositories/infra-repo/openstack/main.tf
+++ kubernetes/fluxcd/repositories/infra-repo/openstack/main.tf
@@ -7,17 +7,21 @@ terraform {
   }
 }
 
-provider "openstack" {
-  # ใช้ค่า OS_* จาก Secret ผ่าน env (ไม่ต้องใส่อะไรเพิ่ม)
-}
+# Uses OS_* from env injected by the Kubernetes Secret (no creds here)
+provider "openstack" {}
+
+# Variables (we'll set via terraform.tfvars or tf-controller values)
+variable "image_id"      { type = string }
+variable "flavor_id"     { type = string }
+variable "network_id"    { type = string }
+variable "keypair_name"  { type = string }
 
 resource "openstack_compute_instance_v2" "vm" {
-  name       = "front-test-1"
-  image_name = "Ubuntu 22.04"     # ปรับตามชื่อ image ที่ NIPA ให้
-  flavor_name = "ani.large.v2"        # ปรับตาม flavor ที่มี
-  key_pair    = "Fronttest"      # ต้องมี keypair นี้ในโปรเจกต์
+  name            = "front-test-1"
+  image_id        = var.image_id
+  flavor_id       = var.flavor_id
+  key_pair        = var.keypair_name
   security_groups = ["default"]
 
-  network { name = "Front-Network" }    # หรือใส่ network_id ตามที่คุณใช้จริง
+  network { uuid = var.network_id }
 }
-
---
+++ kubernetes/fluxcd/repositories/infra-repo/openstack/terraform.tfvars
@@ -0,0 +1,4 @@
+image_id     = "30c876dd-4470-47d8-b13a-df5f18c85ba4"
+flavor_id    = "8d1aecbc-4b18-42cc-b1c7-2c3d002fc8de"
+network_id   = "322bbf78-cebc-45c7-a68a-241d60dbfc01"
+keypair_name = "Fronttest"
